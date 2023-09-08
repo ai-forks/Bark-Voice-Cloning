35d6c6f7e6f4aeebff3c88b48d7b7f5a3a4a26f6
sd <leowest1921@gmail.com>
2023-09-09 07:54:21 +0800

    add

--- app.py
+++ app.py
@@ -309,6 +309,7 @@ while run_server:
                 with gr.Column():
                     #initialname = "/content/Bark-Voice-Cloning/bark/assets/prompts/file"
                     initialname = "./bark/assets/prompts/file"
+                    print(f"===>initialname={initialname}")
                     output_voice = gr.Textbox(label="Filename of trained Voice (do not change the initial name)", lines=1, placeholder=initialname, value=initialname, visible=False)
                 with gr.Column():
                     tokenizerlang = gr.Dropdown(tokenizer_language_list, label="Base Language Tokenizer", value=tokenizer_language_list[1], visible=False)
